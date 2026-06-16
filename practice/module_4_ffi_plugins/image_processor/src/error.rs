124b62c403f27e938897d01c970df65982a615a9
mrakul <micronX@yandex.ru>
2026-06-16 23:29:54 +0300

    [A] Модуль 4: "Механизмы FFI и использование unsafe кода в Rust", Проектная работа "Обработчик изображений с плагинами"
    - Функция-заглушка для применения плагина: загрузка исходного изображения -> rgba8 -> Vec<u8> -> rgba8 (RgbaImabe)-> сохранение той же картинки по другому пути

---
+++ practice/module_4_ffi_plugins/image_processor/src/error.rs
@@ -0,0 +1,20 @@
+
+#[derive(Debug)]
+pub enum ProcessingError {
+    LoadImage(image::ImageError),
+    SaveImage(image::ImageError),
+    ProcessImage(image::ImageError),
+    FileNotFound(String)
+}
+
+// Дисплей для вывода
+impl std::fmt::Display for ProcessingError {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ProcessingError::LoadImage(image_error) => write!(formatter, "Ошибка загрузки изображения, image крат: {}", image_error),
+            ProcessingError::SaveImage(image_error) => write!(formatter, "Ошибка сохранения изображения, image крат: {}", image_error),
+            ProcessingError::ProcessImage(image_error) => write!(formatter, "Ошибка обработки изображения, image крат: {}", image_error),
+            ProcessingError::FileNotFound(path) => write!(formatter, "Файл не найден: {}", path)
+        }
+    }
+}
--- practice/module_4_ffi_plugins/image_processor/src/main.rs
+++ practice/module_4_ffi_plugins/image_processor/src/main.rs
@@ -1,8 +1,11 @@
+mod error;
+use error::ProcessingError;
 use clap::Parser;
 use std::path::PathBuf;
+use image::{ImageBuffer, RgbaImage};
 
 // cargo run -p image_processor
-// cargo run -p image_processor -- --input dummy_input.png --output dummy_output.png --plugin mirror --params dummy_params.txt
+// cargo run -p image_processor -- --input ./aux/random_png_1033x775.png --output ./aux/test_output.png --plugin mirror --params ./aux/dummy_params.txt
 
 #[derive(Parser, Debug)]
 #[command(name = "image_processor", about = "CLI для проверки применения плагинов к изображению")]
@@ -25,8 +28,52 @@ struct Args {
 
     /// Путь к директории с плагинами
     #[arg(long, default_value = "target/debug")]
-    plugin_path: PathBuf,
+    plugin_path: PathBuf
 }
+
+// Обработка плагином
+fn process_image_with_plugin( input_path: &PathBuf, output_path: &PathBuf,
+                            // TODO:
+                            _plugin_name: &str, _plugin_path: &PathBuf,
+                            _params_path: &PathBuf) -> Result<(), ProcessingError> {
+    
+    println!("Загрузка изображения: {}", input_path.display());
+
+    // Загружаем с помощью image::open
+    let image_loaded = image::open(input_path)
+        .map_err(ProcessingError::LoadImage)?;
+
+    // Конвертер в rgba8 (image крат) - все по байту, a - alpha (прозрачность)
+    // (тип: ImageBuffer<image::Rgba<u8>, Vec<u8>>)
+    let rgba_image = image_loaded.to_rgba8();
+
+    // Тюпл ширина / высота
+    let (width, height) = rgba_image.dimensions();
+    println!("Размер изображения: {}x{}", width, height);
+
+    // Перевод в сырой вектор: Vec<u8>, размер должен быть width * height * 4
+    let rgba_buffer: Vec<u8> = rgba_image.into_raw();
+    println!("Размер буфера: {} байт", rgba_buffer.len());
+
+    // TODO: обработка плагином
+
+    // Перевод обратно в RgbaImage (ImageBuffer::from_vec(...) возвращает RgbaImage)
+    // Выдаст ошибку: 
+    // let processed_image: RgbaImage = ImageBuffer::from_vec(10000, 10000, rgba_buffer)
+    let processed_image: RgbaImage = ImageBuffer::from_vec(width, height, rgba_buffer)
+        .ok_or_else(|| {image::ImageError::IoError(std::io::Error::new(std::io::ErrorKind::InvalidData,
+                            "Размеры не соответствуют размеру буфера"))
+    }).map_err(ProcessingError::ProcessImage)?;
+
+
+    // Сохраняем обработанную картинку
+    println!("Обработанное изображение: {}", output_path.display());
+    processed_image.save(output_path)
+        .map_err(ProcessingError::SaveImage)?;
+
+    Ok(())
+}
+
 
 fn main() {
     // С CLAP'ом всё стандартно
@@ -50,5 +97,16 @@ fn main() {
     println!("Файл параметров:\t\t{}", args.params.display());
     println!("Путь к плагинам:\t\t{}", args.plugin_path.display());
 
-    // TODO: основная логика
+    // Обработка запрошенным плагином
+    match process_image_with_plugin(&args.input, &args.output, &args.plugin, &args.plugin_path, &args.params) {
+        Ok(()) => {
+            println!("Изображение успешно обработано плагином: {}", args.plugin);
+        }
+        Err(error) => {
+            eprintln!("Ошибка обработки: {}", error);
+            // std::process::exit(1);
+        }
+    }
+
+
 }
