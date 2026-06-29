f0ef2674bea8ae37e5130d83bd568665a4d0b8f0
mrakul <micronX@yandex.ru>
2026-06-30 01:02:14 +0300

    [F] Модуль 5: "Использование инструментов для отладки и оптимизации кода на Rust", проектная работа "Поиск ошибок и оптимизация"
    Исправления:
      - race_increment_is_correct() - использование атомарной переменной с соответствующими методами
      - test_use_after_free - сначала использование, потом drop, и всё под секцией unsafe, а то Warning'и

--- practice/module_5_errors_optimization/src/concurrency.rs
+++ practice/module_5_errors_optimization/src/concurrency.rs
@@ -1,35 +1,43 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::atomic::{AtomicU64, Ordering};
 
-static mut COUNTER: u64 = 0;
+// static mut COUNTER: u64 = 0;
+static COUNTER: AtomicU64 = AtomicU64::new(0);
 
 /// Небезопасный инкремент через несколько потоков.
 /// Использует global static mut без синхронизации — data race.
 pub fn race_increment(iterations: usize, threads: usize) -> u64 {
-    unsafe { COUNTER = 0; }
+    
+    // unsafe убираем, атомарная переменная с соответствующими операциями, Relaxed достаточно 
+    reset_counter();
+
     let mut handles = Vec::new();
     for _ in 0..threads {
         handles.push(thread::spawn(move || {
             for _ in 0..iterations {
-                unsafe {
-                    COUNTER += 1;
-                }
+                // Инкремент через fetch_add
+                COUNTER.fetch_add(1, Ordering::Relaxed);
             }
         }));
     }
     for h in handles {
         let _ = h.join();
     }
-    unsafe { COUNTER }
+
+    // Возврат через load
+    COUNTER.load(Ordering::Relaxed)
 }
 
 /// Плохая «синхронизация» — просто sleep, возвращает потенциально устаревшее значение.
 pub fn read_after_sleep() -> u64 {
     thread::sleep(Duration::from_millis(10));
-    unsafe { COUNTER }
+    // Аналогично
+    COUNTER.load(Ordering::Relaxed)
 }
 
 /// Сброс счётчика (также небезопасен, без синхронизации).
 pub fn reset_counter() {
-    unsafe { COUNTER = 0; }
+    // Аналогично
+    COUNTER.store(0, Ordering::Relaxed);
 }
