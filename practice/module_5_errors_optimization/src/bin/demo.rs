e9f195fec8583a22957d309abedc02dc57c9b35f
mrakul <micronX@yandex.ru>
2026-07-01 01:47:34 +0300

    [U] Модуль 5: "Использование инструментов для отладки и оптимизации кода на Rust", проектная работа "Поиск ошибок и оптимизация"
    - Сделал цикл, чтобы время выполнения было 3-5 секунд, и при этом сохранились пропорции в стеке-snapshot'е, снимаемом perf'ом
    - Обернул вызовы в black_box, чтобы не соптимизировались
    - Входные данные те же
    - Убрал println'ы
    - Cargo.toml => [profile.release] для отладочных символов
                    debug = 1

--- practice/module_5_errors_optimization/src/bin/demo.rs
+++ practice/module_5_errors_optimization/src/bin/demo.rs
@@ -1,18 +1,22 @@
 use broken_app::{algo, leak_buffer, normalize, sum_even};
 
 fn main() {
-    let nums = [1, 2, 3, 4];
-    println!("sum_even: {}", sum_even(&nums));
+    // - Сделал цикл, чтобы время выполнения было 3-5 секунд, и при этом сохранились пропорции в стеке-snapshot'е, снимаемом perf'ом
+    // - Обернул вызовы в black_box, чтобы не соптимизировались
+    // - Убрал println'ы. Можно оставить, но я убрал :)
+    // - Входные данные те же
+    for _ in 0..=100000
+    {
+        std::hint::black_box(sum_even(&[1, 2, 3, 4]));
 
-    let data = [1_u8, 0, 2, 3];
-    println!("non-zero bytes: {}", leak_buffer(&data));
+        std::hint::black_box(leak_buffer(&[1_u8, 0, 2, 3]));
 
-    let text = " Hello World ";
-    println!("normalize: {}", normalize(text));
+        std::hint::black_box(normalize(" Hello World "));
 
-    let fib = algo::slow_fib(20);
-    println!("fib(20): {}", fib);
+        std::hint::black_box(algo::slow_fib(20));
 
-    let uniq = algo::slow_dedup(&[1, 2, 2, 3, 1, 4, 4]);
-    println!("dedup: {:?}", uniq);
+        std::hint::black_box(algo::slow_dedup(&[1, 2, 2, 3, 1, 4, 4]));
+    }
+
+    println!("Конец обработки");
 }
